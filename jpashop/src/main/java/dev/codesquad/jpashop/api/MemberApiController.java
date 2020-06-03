c85c7fc08614721576ce6bc12f34e653c046abb6
wooody92 <pkower4@naver.com>
2020-06-03 21:27:45 +0900

    [feat] 회원 조회 API Version-2
    - 회원 조회 기능 추가

--- jpashop/src/main/java/dev/codesquad/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/dev/codesquad/jpashop/api/MemberApiController.java
@@ -2,12 +2,15 @@ package dev.codesquad.jpashop.api;
 
 import dev.codesquad.jpashop.domain.Member;
 import dev.codesquad.jpashop.service.MemberService;
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -19,6 +22,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberApiController {
 
   private final MemberService memberService;
+
+  @GetMapping("/api/v1/members")
+  public List<Member> membersV1() {
+    return memberService.findMembers();
+  }
+
+  @GetMapping("/api/v2/members")
+  public Result memberV2() {
+    List<Member> findMembers = memberService.findMembers();
+    List<MemberDto> collect = findMembers.stream()
+        .map(m -> new MemberDto(m.getName()))
+        .collect(Collectors.toList());
+    return new Result(collect.size(), collect);
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class Result<T> {
+
+    private int count;
+    private T data;
+  }
+
+  @Data
+  @AllArgsConstructor
+  static class MemberDto {
+
+    private String name;
+  }
 
   @PostMapping("/api/v1/members")
   public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
@@ -47,12 +79,14 @@ public class MemberApiController {
   @Data
   @AllArgsConstructor
   static class UpdateMemberResponse {
+
     private Long id;
     private String name;
   }
 
   @Data
   static class UpdateMemberRequest {
+
     private String name;
   }
 
