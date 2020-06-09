25cf7c3dfaff939693631f92eeb348b21c468ac3
wooody92 <pkower4@naver.com>
2020-06-09 19:33:12 +0900

    [feat] 조회용 샘플 데이터 입력
    - 스프링 시작 시 이니셜 샘플 데이터 입력

--- jpashop/src/main/java/dev/codesquad/jpashop/domain/Address.java
+++ jpashop/src/main/java/dev/codesquad/jpashop/domain/Address.java
@@ -3,12 +3,20 @@ package dev.codesquad.jpashop.domain;
 import lombok.Getter;
 
 import javax.persistence.Embeddable;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
